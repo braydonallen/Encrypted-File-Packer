052ddefed65e28fa27256822e7d5e8efadec72a6
Braydon Allen <142241682+braydonallen@users.noreply.github.com>
2025-09-03 13:00:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,97 @@
 #!/usr/bin/env python
 from cryptography.fernet import Fernet
-import base64, gzip
-import sys
+import gzip, sys, math, random, keyword, builtins, string
 
-code = '''#!/usr/bin/env python
-from cryptography.fernet import Fernet
-import base64, gzip
+OPTIONS = {
+    "encode": [
+        "encode()",
+        "encode('utf-8')",
+        "encode(errors='strict')",
+        "encode('utf8')",
+        "encode('UTF-8')",
+        "encode('cp65001')",
+    ],
 
-blob = {1}{0}{1}
-key = {1}{2}{1}
+    "decode": [
+        "decode()",
+        "decode('utf-8')",
+        "decode(errors='strict')",
+        "decode('utf8')",
+        "decode('UTF-8')",
+        "decode('cp65001')",
+    ],
 
-f = Fernet(key)
+    "decrypt": [
+        "decrypt",
+    ],
 
-code = blob.encode('utf-8')
-code = f.decrypt(code)
-code = gzip.decompress(code)
-code = code.decode('utf-8')
+    "cryptography": [
+        "import cryptography.fernet",
+        "from cryptography import fernet",
+    ],
 
-exec(code)
-'''
+    "alias": [],
+
+    "decompress": [
+        "decompress",
+        "decompress ",
+    ],
+}
+
+Chosen = {}
+
+def random_alias(length=3):
+    while True:
+        first = random.choice(string.ascii_letters)
+        rest = ''.join(random.choices(string.ascii_letters + string.digits + "_", k=length-1))
+        name = first + rest
+        if not keyword.iskeyword(name) and not hasattr(builtins, name):
+            return name
+
+def GetRandom(opt):
+    if opt == "alias":
+        return random_alias(random.randint(2,5))  # random length 1–4
+    return random.choice(OPTIONS[opt])
+
+for i in OPTIONS:
+    Chosen[i] = GetRandom(i)
+
+code_tpl = (
+    "{crypt} as {alias};"
+    "import gzip;"
+    "exec(gzip.{decomp}({alias}.Fernet({key!r}).{decrypt}({cipher!r}.{encode})).{decode})"
+)
+
+code_base = code_tpl.format(
+    crypt=Chosen["cryptography"],
+    alias=Chosen["alias"],
+    decomp=Chosen["decompress"],
+    decrypt=Chosen["decrypt"],
+    encode=Chosen["encode"],
+    decode=Chosen["decode"],
+    key="{KEY}",
+    cipher="{CIPHER}",
+)
 
 if len(sys.argv) != 2:
-    print(f'err: incorrent num of arguments (expected 2, got {len(sys.argv)})')
+    print(f'err: incorrect num of arguments (expected 2, got {len(sys.argv)})')
     exit(-1)
 
 with open(sys.argv[1], 'r') as file:
     contents = file.read()
 
-    txt = contents.encode('utf-8')
+    txt = contents.encode()
     txt = gzip.compress(txt)
 
     key = Fernet.generate_key()
     f = Fernet(key)
     txt = f.encrypt(txt)
 
-    txt = txt.decode('utf-8')
+    txt = txt.decode()
 
-    txt = code.format(txt, "'''", key.decode('utf-8'))
+    final_code = code_base.format(
+        KEY=key.decode(),
+        CIPHER=txt
+    )
 
-    print(txt)
+    print(final_code)
